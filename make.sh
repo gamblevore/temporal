7ba90b7320190b1dbbb9e9ab7cbd77d318b234d6
gamblevore <theo@jeebox.org>
2020-03-09 17:27:27 +0000

    update

--- make.sh
+++ make.sh
@@ -11,7 +11,7 @@ g++  -fPIC  -std=c++0x  -O0  -c  "$DIR/Src/gen.cpp"
 g++  -fPIC  -std=c++0x  -Os  -c  "$DIR/Src/temporal.cpp"
 g++  -fPIC  -std=c++0x  -Os  -c  "$DIR/Src/lib.cpp"
 cd ..
-g++  -pthread build/gen.o build/temporal.o -o result/temporal
+g++  -fPIC  -pthread build/gen.o build/temporal.o -o result/temporal
 ar   rcs      result/TemporalLib.a build/gen.o build/lib.o
 cd "$ORIG"
 
