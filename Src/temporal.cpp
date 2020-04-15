a24e15b7f42c1dcbf8e67fa226e1cc8530371e6d
gamblevore <theo@jeebox.org>
2020-04-15 15:17:22 +0100

    removed test

--- Src/temporal.cpp
+++ Src/temporal.cpp
@@ -4,8 +4,8 @@
 #include "temporal_root.i"
 
 int main (int argc, const char* argv[]) {
-	DummyIPhoneTest();
-	return 0;
+//	DummyIPhoneTest();
+//	return 0;
 	return bh_run_command(nullptr, argv, false);
 }
 
